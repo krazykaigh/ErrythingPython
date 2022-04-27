ed1b936955ac2d7c0dae88670598cb8c25005230
Kaigh Taylor <coders@kai-deas.com>
2022-04-27 15:18:56 -0400

    erryPy | updated output

--- Health.py
+++ Health.py
@@ -1,10 +1,11 @@
 import random
 
 health =  50
+print('Health before picking up potion = {}'.format(health))
 
 potion_health = random.randint(25, 50)
 print('Potion health = {}'.format(potion_health))
 
 health += potion_health
 
-print('Health after picking up potion = l{}'.format(health))
+print('Health after picking up potion = {}'.format(health))
