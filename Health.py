956376bcf84009526d16a6b4791beb8ba56b6cdf
Kaigh Taylor <coders@kai-deas.com>
2022-04-27 21:57:13 -0400

    erryPy | Updated  capabilities

--- Health.py
+++ Health.py
@@ -1,9 +1,24 @@
 import random
 
+""" Set Difficulty Level """
+
+difficulty = random.randint(1,3)
+
 health =  50
 print('Health before picking up potion = {}'.format(health))
 
-potion_health = random.randint(25, 50)
+if difficulty == 1:
+    print('Difficulty Level = Easy')
+    potion_health = random.randint(25, 50)
+
+elif difficulty == 2:
+    print('Difficulty Level = Normal')
+    potion_health = random.randint(15, 25)
+
+else:
+    print('Difficulty Level = Hard')
+    potion_health = random.randint(5, 15)
+
 print('Potion health = {}'.format(potion_health))
 
 health += potion_health
