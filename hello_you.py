2443ddade790d8a851a02b2734d2b8f1b30fff61
Kaigh Taylor <coders@kai-deas.com>
2022-04-30 19:17:24 -0400

    erryPy | adding input

---
+++ hello_you.py
@@ -0,0 +1,14 @@
+# Ask user for name
+name = input("What is your name: ")
+print(f"{name} is a very nice name!")
+# Ask user for age
+age = input (f"{name} what is your age: ")
+print(f"Well {name} it's nice to know that you are {age} years old")
+
+# Ask user for city
+
+# Ask user for what they enjoy
+
+# Create output text
+
+# Print output to screen
