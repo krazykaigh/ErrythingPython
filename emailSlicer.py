91c5a7b68f04373eef8b0f48de821ee69e51960f
Kaigh Taylor <coders@kai-deas.com>
2022-05-02 13:41:49 -0400

    errypy Slicer : Practice index, slicing

--- emailSlicer.py
+++ emailSlicer.py
@@ -3,5 +3,16 @@ word = "supercalifragilisticexpialidocious"
 # print(f"{}"[(word[5:10:2])])
 sliceOfWord = (word[4:9:1])
 print("{}".format(sliceOfWord))
+# rcali
 
-print("{}".format(word[7:12:1]))
+print("{}".format(word[5:12:1]))
+# califra
+
+print("{}".format(word[word.index("per"):word.index("ex")]))
+# percalifragilistic
+
+print("{}".format(word[word.index("per"):word.index("e")]))
+# p
+
+sliceOfWord = (word[::-1])
+print("{}".format(sliceOfWord))
