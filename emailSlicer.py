1d5310f013a555467d7adfc054ce0d62a16f4ab9
Kaigh Taylor <coders@kai-deas.com>
2022-05-02 21:22:16 -0400

    erryPy emailSlicer | finalized slice & format

--- emailSlicer.py
+++ emailSlicer.py
@@ -1,3 +1,8 @@
+# How Slicers Work
+
+from xml import dom
+
+
 word = "supercalifragilisticexpialidocious"
 
 # print(f"{}"[(word[5:10:2])])
@@ -16,3 +21,35 @@ print("{}".format(word[word.index("per"):word.index("e")]))
 
 sliceOfWord = (word[::-1])
 print("{}".format(sliceOfWord))
+
+# here is a very long word
+
+word = "antidisestablishmentarianism"
+
+# use a slice to take out the word "establishment"
+# and store it in the answer variable....
+
+answer = word[word.index("e"):word.index("ari")]
+print("{}".format(answer))
+
+# ---------- Email Slicer Program --------------
+
+# get user email address
+email = input("Enter complete email address : ")
+
+# slice out user name
+userName = email[:email.index("@")]
+print(f"User name = {userName}")
+
+# slice domain name
+domainName = email[(email.index("@")+1):]
+print(f"Domain name = {domainName}")
+
+# format message
+output = "Your username is {0}, and your domain name is {1}".format(userName, domainName)
+
+
+# display output message
+print(output)
+
+
