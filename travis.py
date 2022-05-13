065f7a2585e360dae9b92540fe92cce242a33153
Kaigh Taylor <coders@kai-deas.com>
2022-05-13 17:42:01 -0400

    erryPy_Travis | Init

---
+++ travis.py
@@ -0,0 +1,7 @@
+known_users = ['Alice', 'Blobert', 'Zeral', 'Keerub', 'Myrr-Terenial', 'Kai']
+
+print(f'known_users = {known_users}')
+
+while True:
+    print('Hi! My name is Travis')
+    name = input()
