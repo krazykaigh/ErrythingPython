91418ede833835f04db93da190d001b3d61a18ed
Kaigh Taylor <coders@kai-deas.com>
2022-05-13 11:38:04 -0400

    erryPy_lists | added table

--- lists.py
+++ lists.py
@@ -15,3 +15,5 @@ print (f'\nnew_list = {new_list}')
 print ('\nHere\'s a slice from new_list. I will extract the second item of each inner list:')
 print(f'Inner list item from new_list[3][1]: {new_list[3][1]}')
 print(f'Inner list item from new_list[4][1]: {new_list[4][1]}')
+
+our_table = [[1,2,3],[4,5,6],[7,8,9]]
