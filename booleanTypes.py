7613cf449363b8b5687c9d6ea040cc99bef4626b
Kaigh Taylor <coders@kai-deas.com>
2022-05-13 06:04:43 -0400

    erryPy_Boolean  |  logic expressions

---
+++ booleanTypes.py
@@ -0,0 +1,13 @@
+from tkinter import E
+
+
+num1 = 150
+num2 = 150
+
+if num1 > num2:
+    print(f'{num1} is greater than {num2}')
+elif num2 > num1:
+    print(f'{num2} is greater than {num1}')
+elif num1 == num2:
+    print(f'{num1} is equal to {num2}')
+
